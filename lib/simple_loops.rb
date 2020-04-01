ca3feb648a4cd1ab5c7212bab0d4f16ad9875bd1
Taiye Salami <taiye41@gmail.com>
2020-04-01 11:44:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,12 @@
 # Write your methods here
 def loop_message_five_times(message)
-  5.times do
-    message
+  i = 0
+  while i < 5
+    puts message
+    i += 1
   end
+  # 
+  # 5.times do
+  #   message
+  # end
 end
